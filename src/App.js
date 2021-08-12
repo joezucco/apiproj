542a2d1ceffc149d6378f8c259882c1bd99cd57d
Joe Zucco <josephzucco@gmail.com>
2021-08-12 09:51:38 -0400

    syntax

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import { Route, Switch } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "./App.css";
-import "./components/About"
+import "./components/About";
 
 import Header from "./components/Header";
 import About from "./components/About";
 import Home from "./components/Home";
 
 function App() {
-
   const url =
     "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=300";
-  const [catFacts, setCatFacts] = useState([])
-  
-  const getCatFacts = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    setCatFacts(data)
-  }
+  const [catFacts, setCatFacts] = useState([]);
 
-  useEffect(() => {getCatFacts()}, []) 
+  const getCatFacts = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setCatFacts(data);
+  };
+
+  useEffect(() => {
+    getCatFacts();
+  }, []);
 
   return (
-    <div className="App">
-      <div className="site">
+    <div className='App'>
+      <div className='site'>
         <Header />
-        <main className="site">
+        <main className='site'>
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Home catFacts={catFacts} />
             </Route>
-            <Route path="/about">
+            <Route path='/about'>
               <About />
             </Route>
           </Switch>
